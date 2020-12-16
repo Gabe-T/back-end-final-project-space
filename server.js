03f27a70374205082232e27965455a42187820f7
Gabe-T <gabriel.tarrant@hotmail.com>
2020-12-16 11:34:00 -0500

    initial commit

--- server.js
+++ server.js
@@ -10,8 +10,12 @@ app.use(cors());
 app.use(express.json());
 app.use("/", routes);
 // app.use("/", );
-
-const port = 3000;
-app.listen(port, () => {
-  console.log(`listening on port: ${port}`);
-});
+// define the port
+const DEFAULT_PORT = 3000;
+// Use Heroku's PORT or default to 3000.
+const port = process.env.PORT || DEFAULT_PORT;
+// run the server
+app.listen(port, () =>
+  console.log(`Listening on
+port: ${port}.`)
+);
